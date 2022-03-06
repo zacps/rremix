7f6cc9e9d2a2350f4126efe55c9f557ad38452c3
Zac Pullar-Strecker <zacmps@gmail.com>
2022-03-06 13:09:13 +1300

    Parser improvements & first few passing tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,29 @@
+use pest::{iterators::Pair, RuleType};
+
 pub mod parser;
+
+pub fn format_pair<T: RuleType + std::fmt::Debug>(pair: Pair<T>, indent_level: usize, is_newline: bool) -> String {
+    let indent = if is_newline {
+        "  ".repeat(indent_level)
+    } else {
+        "".to_string()
+    };
+
+    let children: Vec<_> = pair.clone().into_inner().collect();
+    let len = children.len();
+    let children: Vec<_> = children.into_iter().map(|pair| {
+        format_pair(pair, if len > 1 { indent_level + 1 } else { indent_level }, len > 1)
+    }).collect();
+
+    let dash = if is_newline {
+        "- "
+    } else {
+        ""
+    };
+
+    match len {
+        0 => format!("{}{}{:?}: {:?}", indent, dash, pair.as_rule(), pair.into_span().as_str()),
+        1 => format!("{}{}{:?} > {}", indent, dash, pair.as_rule(), children[0]),
+        _ => format!("{}{}{:?}\n{}", indent, dash, pair.as_rule(), children.join("\n"))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{fs, fmt::Display};
 
 use pest::{Parser, iterators::Pair, RuleType};
-use rremix::parser::{RemixParser, Rule};
+use rremix::{parser::{RemixParser, Rule}, format_pair};
 
 fn main() {
     let file = fs::read_to_string("test.rem").expect("failed to read test.rem");
@@ -10,30 +10,4 @@ fn main() {
         &file
     ).expect("failed parse");
     print!("{}", parsed.map(|pair| format_pair(pair, 0, true)).collect::<Vec<_>>().join("\n"));
-}
-
-fn format_pair<T: RuleType + std::fmt::Debug>(pair: Pair<T>, indent_level: usize, is_newline: bool) -> String {
-    let indent = if is_newline {
-        "  ".repeat(indent_level)
-    } else {
-        "".to_string()
-    };
-
-    let children: Vec<_> = pair.clone().into_inner().collect();
-    let len = children.len();
-    let children: Vec<_> = children.into_iter().map(|pair| {
-        format_pair(pair, if len > 1 { indent_level + 1 } else { indent_level }, len > 1)
-    }).collect();
-
-    let dash = if is_newline {
-        "- "
-    } else {
-        ""
-    };
-
-    match len {
-        0 => format!("{}{}{:?}: {:?}", indent, dash, pair.as_rule(), pair.into_span().as_str()),
-        1 => format!("{}{}{:?} > {}", indent, dash, pair.as_rule(), children[0]),
-        _ => format!("{}{}{:?}\n{}", indent, dash, pair.as_rule(), children.join("\n"))
-    }
 }
